88abbc1e7a4f183b271c1bf4c00baf5c2f740a8f
SahikaErcan <ercansahika23946@gmail.com>
2021-04-24 05:46:34 +0300

    24 Nisan 2021

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -29,7 +29,7 @@ namespace EntityLayer.Concrete
 
         public ICollection<Heading> Headings { get; set; }
 
-       public ICollection<Content> Contents { get; set; }
+        public ICollection<Content> Contents { get; set; }
 
     }
 }
