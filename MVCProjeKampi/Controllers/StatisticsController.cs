e94d3e1b1ae7d75c1fa952d5155e19a125c08e66
SahikaErcan <ercansahika23946@gmail.com>
2021-05-13 21:00:22 +0300

    13 Mayıs 2021

--- MVCProjeKampi/Controllers/StatisticsController.cs
+++ MVCProjeKampi/Controllers/StatisticsController.cs
@@ -12,16 +12,20 @@ namespace MVCProjeKampi.Controllers
         Context context = new Context();
         public ActionResult Index()
         {
-            var value1 = context.Categories.Count().ToString(); // Toplam kategori sayısı
+            // Toplam kategori sayısı
+            var value1 = context.Categories.Count().ToString(); 
             ViewBag.sumCategory = value1;
-            var value2 = context.Headings.Count(x => x.CategoryID == 8).ToString(); // Başlık tablosunda "yazılım" kategorisine ait başlık sayısı
+            // Başlık tablosunda "yazılım" kategorisine ait başlık sayısı
+            var value2 = context.Headings.Count(x => x.CategoryID == 8).ToString(); 
             ViewBag.softwareTitle = value2;
-            var value3 = context.Writers.Count(x => x.WriterName.Contains("a")).ToString(); // Yazar adında 'a' harfi geçen yazar sayısı
+            // Yazar adında 'a' harfi geçen yazar sayısı
+            var value3 = context.Writers.Count(x => x.WriterName.Contains("a")).ToString(); 
             ViewBag.writerNameLetterA = value3;
-            var value4 = context.Headings.Max(x => x.Category.CategoryName); // En fazla başlığa sahip kategori adı
+            // En fazla başlığa sahip kategori adı
+            var value4 = context.Categories.Where(c => c.CategoryID == context.Headings.GroupBy(x => x.CategoryID).OrderByDescending(y => y.Count()).Select(z => z.Key).FirstOrDefault()).Select(n => n.CategoryName).FirstOrDefault(); 
             ViewBag.categoryNameMore = value4;
-            var value5 = context.Categories.Count(x => x.CategoryStatus == true) - context.Categories.Count(x => x.CategoryStatus == false); 
             //Kategori tablosunda durumu true olan kategoriler ile false olan kategoriler arasındaki sayısal fark
+            var value5 = context.Categories.Count(x => x.CategoryStatus == true) - context.Categories.Count(x => x.CategoryStatus == false); 
             ViewBag.activeCategory = value5;
             return View();
         }
